a724612731165787e0a1c397bee5c1a32ef497f2
pehladik <pehladik@gmail.com>
2020-03-28 17:27:42 +0100

    add v4l2loopback start module to the starter script

--- start.sh
+++ start.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
-# getwindidbypid
-# 
-# Get the ID of a window by PID (if the process has a window).
-# 
 # Usage:
-#   getwindidbypid <PID>
+#   start <v4l2loopback_path>
 # 
 
-./dumberdore.x86_64 & export DUMBER_PID=$!
-echo "dumby launched"
-cpulimit --pid ${DUMBER_PID} -l 50 &
+if [ "$#" -ne 1 ]; then
+ echo "Add path of v4l2loopback folder as parameter"
+else
+ LOCAL=${PWD}
 
-sleep 1
-echo ${DUMBER_PID}
-while IFS= read line; do
+ sudo modprobe v4l2loopback
+ sudo rmmod v4l2loopback
+ cd ${1}
+ sudo insmod v4l2loopback.ko max_buffers=2
+
+ cd ${LOCAL}
+ ./dumberdore.x86_64 & export DUMBER_PID=$!
+ cpulimit --pid ${DUMBER_PID} -l 50 &
+ echo "dumby launched"
+
+ sleep 1
+ echo ${DUMBER_PID}
+ while IFS= read line; do
   if [[ "${line}" =~ (0x)([0-9a-z]+)([ ][- ][0-9]+[ ])([0-9]*) ]]; then
     winId="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
     pid="${BASH_REMATCH[4]}"
@@ -21,13 +28,15 @@ while IFS= read line; do
       WIND_IDS+=("${winId}")
     fi
   fi
-done < <(wmctrl -lp)
+ done < <(wmctrl -lp)
 
-if [ "${#WIND_IDS[@]}" -gt 0 ]; then
+ if [ "${#WIND_IDS[@]}" -gt 0 ]; then
   echo "xid is ${WIND_IDS[0]}"
+ fi
+
+ #v4l2loopback-ctl set-caps "video/x-raw,format=UYVY,width=640,height=480" /dev/video0
+ v4l2loopback-ctl set-fps 30 /dev/video0
+ gst-launch-1.0 -v ximagesrc xid=${WIND_IDS[0]} ! videoconvert ! videoscale ! video/x-raw,format=UYVY,width=640,height=480,framerate=30/1 ! identity drop-allocation=1 ! v4l2sink device=/dev/video0
 fi
-
-gst-launch-1.0 -v ximagesrc xid=${WIND_IDS[0]} ! videoconvert ! videoscale ! video/x-raw,format=UYVY,width=640,height=480,framerate=30/1 ! identity drop-allocation=1 ! v4l2sink device=/dev/video0
-
 
 
